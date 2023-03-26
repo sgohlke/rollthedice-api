6f1be71e6b2c7053a4b95b92313d52629ce07ce6
stefangohlke <gohlke.stefan@yahoo.de>
2023-03-26 22:36:10 +0200

    Update deps, use deno-web

--- webserver.ts
+++ webserver.ts
@@ -1,56 +1,57 @@
-import { serve } from './deps.ts';
+import {
+   JSON_CONTENT_TYPE_HEADER,
+   returnDataResponse,
+   startServer,
+} from './deps.ts'
 
-const port = 3018;
+const port = 3018
 
 function generateRandomNumber() {
-   const randomNumber = Math.random() * 6;
+   const randomNumber = Math.random() * 6
    //console.log('Random number is', randomNumber);
-   return Math.ceil(randomNumber);
+   return Math.ceil(randomNumber)
 }
 
 function generateRandomNumbers(numberCount: number): Array<number> {
-   const resultArray: Array<number> = [];
+   const resultArray: Array<number> = []
    if (numberCount > 1000) {
-      numberCount = 1000;
+      numberCount = 1000
    }
    for (let index = 0; index < numberCount; index++) {
-      resultArray.push(generateRandomNumber());
+      resultArray.push(generateRandomNumber())
    }
-   return resultArray;
+   return resultArray
 }
 
 function handleRequest(request: Request): Response {
-   const responseHeaders = new Headers();
-   responseHeaders.set('content-type', 'application/json; charset=UTF-8');
+   const responseHeaders = new Headers(JSON_CONTENT_TYPE_HEADER)
 
-   const origin = request.headers.get('origin');
+   const origin = request.headers.get('origin')
    if (origin) {
-      responseHeaders.set('Access-Control-Allow-Origin', origin);
+      responseHeaders.set('Access-Control-Allow-Origin', origin)
    }
 
    if (request.method === 'OPTIONS') {
       //responseHeaders.set('Access-Control-Allow-Headers', 'Authorization');
-      return new Response(undefined, { headers: responseHeaders });
+      return new Response(undefined, { headers: responseHeaders })
    } else {
-      const { pathname } = new URL(request.url);
+      const { pathname } = new URL(request.url)
       if (pathname.includes('/random/')) {
-         const urlParam = pathname.substring(8);
+         const urlParam = pathname.substring(8)
          if (urlParam) {
-            const numberOfRandomNumbers = parseInt(urlParam);
+            const numberOfRandomNumbers = parseInt(urlParam)
             if (numberOfRandomNumbers) {
-               return new Response(
-                  JSON.stringify({
-                     numbers: generateRandomNumbers(numberOfRandomNumbers),
-                  }),
-                  { headers: responseHeaders },
-               );
+               return returnDataResponse({
+                  numbers: generateRandomNumbers(numberOfRandomNumbers),
+               }, responseHeaders)
             }
          }
       }
-      return new Response(JSON.stringify({ message: 'Hello stackstream!' }), {
-         headers: responseHeaders,
-      });
+      return returnDataResponse({
+         message:
+            'Welcome to Rollthedice API!',
+      }, responseHeaders)
    }
 }
 
-serve(handleRequest, { port: port });
+startServer(handleRequest, { port: port })
